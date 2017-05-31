7a0df4b4da821ba1ce4ff8ea830f6f0364e669ec
miriam-lau <meiyen.lau@gmail.com>
2017-05-30 23:29:51 -0700

    Sounds with keypresses working

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
+var webpack = require('webpack');
+
 module.exports = {
   entry: "./lib/lullaby.js",
   output: {
     filename: "./lib/bundle.js"
   },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    })
+  ],
   devtool: 'source-map',
 };
